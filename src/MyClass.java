c62126114f7a666a9fa7b0f5792ec933eec274ed
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-12 11:47:18 +0530

    Content Updated

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
-public class MyClass {
+
+@WebServlet(urlPatterns = "/customer")
+public class MyClass extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Requested");
+    }
 }
