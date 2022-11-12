c043d4eb959419335afcba4a3aa82a1a58cbd76d
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-12 13:30:28 +0530

    Content Updated

--- 03_Simple-App/src/Customer.java
+++ 03_Simple-App/src/Customer.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author : Sandun Induranga
@@ -15,6 +16,12 @@ public class Customer extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String cusId = req.getParameter("cusId");
-        System.out.println(cusId);
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
+        String cusSalary = req.getParameter("cusSalary");
+
+        PrintWriter writer = resp.getWriter();
+        writer.write(cusId + cusName + cusAddress + cusSalary);
+
     }
 }
