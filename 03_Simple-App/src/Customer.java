feea89a000157fc922ce0666e05634380815a30d
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-12 12:48:23 +0530

    Content Updated

--- 03_Simple-App/src/Customer.java
+++ 03_Simple-App/src/Customer.java
@@ -1,6 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
-public class Customer {
+
+@WebServlet(urlPatterns = "/customer")
+public class Customer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cusId = req.getParameter("cusId");
+        System.out.println(cusId);
+    }
 }
