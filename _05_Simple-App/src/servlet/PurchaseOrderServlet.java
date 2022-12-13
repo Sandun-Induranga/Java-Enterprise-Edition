d9dadd199395040ef23050d3980c71c9bbb60016
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-13 12:01:15 +0530

    Content Updated

--- _05_Simple-App/src/servlet/PurchaseOrderServlet.java
+++ _05_Simple-App/src/servlet/PurchaseOrderServlet.java
@@ -1,7 +1,11 @@
 package servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author : Sandun Induranga
@@ -10,4 +14,8 @@ import javax.servlet.http.HttpServlet;
 
 @WebServlet("/order")
 public class PurchaseOrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
