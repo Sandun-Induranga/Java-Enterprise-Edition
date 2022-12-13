7628d45a897626e4cef5a7e92e8597a4a423804f
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-13 12:17:46 +0530

    Content Updated

--- _05_Simple-App/src/servlet/PurchaseOrderServlet.java
+++ _05_Simple-App/src/servlet/PurchaseOrderServlet.java
@@ -1,14 +1,16 @@
 package servlet;
 
+import model.CustomerDTO;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * @author : Sandun Induranga
@@ -22,8 +24,25 @@ public class PurchaseOrderServlet extends HttpServlet {
         String cusId = req.getParameter("cusId");
 
         try {
+
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/POS","sandu","1234");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/POS", "sandu", "1234");
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer WHERE customerId=?");
+            pstm.setString(1, cusId);
+            ResultSet resultSet = pstm.executeQuery();
+
+            if (resultSet.next()){
+                JsonObjectBuilder obj = Json.createObjectBuilder();
+                obj.add("cusId",resultSet.getString(1));
+                obj.add("cusName",resultSet.getString(2));
+                obj.add("cusAddress",resultSet.getString(3));
+                obj.add("cusSalary",resultSet.getString(4));
+
+                obj.add("state", "OK");
+                obj.add("message", "Successfully Loaded..!");
+                obj.add("data", obj.build());
+                resp.setStatus(200);
+            }
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
