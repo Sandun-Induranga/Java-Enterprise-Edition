c6f26dedd93f5234c08939cedf01cc793d34efc6
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-13 12:06:28 +0530

    Content Updated

--- _05_Simple-App/src/servlet/PurchaseOrderServlet.java
+++ _05_Simple-App/src/servlet/PurchaseOrderServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * @author : Sandun Induranga
@@ -16,6 +19,17 @@ import java.io.IOException;
 public class PurchaseOrderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String cusId = req.getParameter("cusId");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/POS","sandu","1234");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
